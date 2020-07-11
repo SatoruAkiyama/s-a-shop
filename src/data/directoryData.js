f676d50577d5f4acdfcc8187cb42b8795d7be2d4
satoru <65771625+SatoruAkiyama@users.noreply.github.com>
2020-07-11 10:59:11 +0900

    added homepage

---
+++ src/data/directoryData.js
@@ -0,0 +1,41 @@
+const DIRECTORY_DATA = [
+  {
+    title: "hats",
+    imageUrl:
+      "https://images.pexels.com/photos/755023/pexels-photo-755023.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    id: 1,
+    linkUrl: "hats",
+  },
+  {
+    title: "jackets",
+    imageUrl:
+      "https://images.pexels.com/photos/246367/pexels-photo-246367.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    id: 2,
+    linkUrl: "jackets",
+  },
+  {
+    title: "sneakers",
+    imageUrl:
+      "https://images.pexels.com/photos/2421374/pexels-photo-2421374.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    id: 3,
+    linkUrl: "sneakers",
+  },
+  {
+    title: "womens",
+    imageUrl:
+      "https://images.pexels.com/photos/871494/pexels-photo-871494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    size: "large",
+    id: 4,
+    linkUrl: "womens",
+  },
+  {
+    title: "mens",
+    imageUrl:
+      "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940s",
+    size: "large",
+    id: 5,
+    linkUrl: "mens",
+  },
+];
+
+export default DIRECTORY_DATA;
