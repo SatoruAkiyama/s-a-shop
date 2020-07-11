c146ddd930099762bfbf93797cd3701e0c2bf3dd
satoru <65771625+SatoruAkiyama@users.noreply.github.com>
2020-07-11 12:49:47 +0900

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./page/home-page/HomePage";
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="container">
-        <HomePage />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
