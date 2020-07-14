940e8902855ab3a9a3c8c27c7f54d6db99c55bfc
satoru <65771625+SatoruAkiyama@users.noreply.github.com>
2020-07-14 14:46:58 +0900

    added withRouter to CollectionPreview component to route each Collection Page

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./CollectionPreview.scss";
 
 import CollectionItem from "../collection-item/CollectionItem";
+import Button from "../button/Button";
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, history, match, routeName }) => {
   return (
     <div className="collection-preview">
-      <h1>{title.toUpperCase()}</h1>
+      <div className="collection-title">
+        {" "}
+        <h1 onClick={() => history.push(`${match.url}/${routeName}`)}>
+          {title.toUpperCase()}
+        </h1>
+      </div>
+
       <div className="preview">
         {items.slice(0, 4).map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
+      <div className="btn">
+        <Button
+          value="MORE"
+          inverted
+          onClick={() => history.push(`${match.url}/${routeName}`)}
+        />
+      </div>
     </div>
   );
 };
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
