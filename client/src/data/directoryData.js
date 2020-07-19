7b01420c3aedf73bb3382a56386bc5ebd5fe7583
satoru <65771625+SatoruAkiyama@users.noreply.github.com>
2020-07-19 21:38:05 +0900

    modified styles in homepage

--- client/src/data/directoryData.js
+++ client/src/data/directoryData.js
@@ -1,17 +1,19 @@
 const DIRECTORY_DATA = [
   {
-    title: "hats",
+    title: "womens",
     imageUrl:
-      "https://images.pexels.com/photos/1474213/pexels-photo-1474213.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/2467393/pexels-photo-2467393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    size: "large",
     id: 1,
-    linkUrl: "shop/hats",
+    linkUrl: "shop/womens",
   },
   {
-    title: "bags",
+    title: "mens",
     imageUrl:
-      "https://images.pexels.com/photos/1117272/pexels-photo-1117272.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/3039183/pexels-photo-3039183.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    size: "large",
     id: 2,
-    linkUrl: "shop/bags",
+    linkUrl: "shop/mens",
   },
   {
     title: "shoes",
@@ -21,20 +23,18 @@ const DIRECTORY_DATA = [
     linkUrl: "shop/shoes",
   },
   {
-    title: "womens",
+    title: "hats",
     imageUrl:
-      "https://images.pexels.com/photos/2467393/pexels-photo-2467393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    size: "large",
+      "https://images.pexels.com/photos/1474213/pexels-photo-1474213.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     id: 4,
-    linkUrl: "shop/womens",
+    linkUrl: "shop/hats",
   },
   {
-    title: "mens",
+    title: "bags",
     imageUrl:
-      "https://images.pexels.com/photos/3039183/pexels-photo-3039183.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    size: "large",
+      "https://images.pexels.com/photos/1039518/pexels-photo-1039518.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     id: 5,
-    linkUrl: "shop/mens",
+    linkUrl: "shop/bags",
   },
 ];
 
